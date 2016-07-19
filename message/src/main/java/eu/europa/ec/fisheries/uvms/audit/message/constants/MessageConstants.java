af837fd8c3924012f67906343755d0c65c731dc3
Joakim Johansson <joakim.johansson@havochvatten.se>
2016-07-19 15:22:09 +0200

    Module can deploy on Weblogic

--- message/src/main/java/eu/europa/ec/fisheries/uvms/audit/message/constants/MessageConstants.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/audit/message/constants/MessageConstants.java
@@ -1,25 +1,25 @@
 /*
-﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
-© European Union, 2015-2016.
+ ﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+ © European Union, 2015-2016.
 
-This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
-redistribute it and/or modify it under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
-the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
-copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+ redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+ the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+ copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
  */
 package eu.europa.ec.fisheries.uvms.audit.message.constants;
 
 public class MessageConstants {
 
-    public static final String CONNECTION_FACTORY = "java:/ConnectionFactory";
+    public static final String CONNECTION_FACTORY = "ConnectionFactory";
     public static final String CONNECTION_TYPE = "javax.jms.MessageListener";
     public static final String DESTINATION_TYPE_QUEUE = "javax.jms.Queue";
-    public static final String AUDIT_MESSAGE_IN_QUEUE = "java:/jms/queue/UVMSAuditEvent";
+    public static final String AUDIT_MESSAGE_IN_QUEUE = "jms/queue/UVMSAuditEvent";
     public static final String AUDIT_MESSAGE_IN_QUEUE_NAME = "UVMSAuditEvent";
-    public static final String AUDIT_RESPONSE_QUEUE = "java:/jms/queue/UVMSAudit";
-    public static final String QUEUE_DATASOURCE_INTERNAL = "java:/jms/queue/UVMSAuditModel";
+    public static final String AUDIT_RESPONSE_QUEUE = "jms/queue/UVMSAudit";
+    public static final String QUEUE_DATASOURCE_INTERNAL = "jms/queue/UVMSAuditModel";
 
     public static final String MODULE_NAME = "audit";
 
--- message/src/main/java/eu/europa/ec/fisheries/uvms/audit/message/producer/bean/MessageProducerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/audit/message/producer/bean/MessageProducerBean.java
@@ -1,13 +1,13 @@
 /*
-﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
-© European Union, 2015-2016.
+ ﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+ © European Union, 2015-2016.
 
-This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
-redistribute it and/or modify it under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
-the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
-copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+ redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+ the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+ copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
  */
 package eu.europa.ec.fisheries.uvms.audit.message.producer.bean;
 
@@ -22,13 +22,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
-import javax.ejb.Stateless;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
 import javax.jms.*;
 
-@Stateless
+@Singleton
 public class MessageProducerBean implements MessageProducer, ConfigMessageProducer {
 
     final static Logger LOG = LoggerFactory.getLogger(MessageProducerBean.class);
@@ -41,45 +41,36 @@ public class MessageProducerBean implements MessageProducer, ConfigMessageProduc
 
     @Resource(mappedName = ConfigConstants.CONFIG_MESSAGE_IN_QUEUE)
     private Queue configQueue;
-    
-//    @Resource(lookup = MessageConstants.CONNECTION_FACTORY)
-//    private ConnectionFactory connectionFactory;
-//
-//    private Connection connection = null;
-//    private Session session = null;
 
     private static final int CONFIG_TTL = 30000;
 
-    @Inject
+    @EJB
     JMSConnectorBean connector;
 
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public String sendDataSourceMessage(String text, DataSourceQueue queue) throws AuditMessageException {
         try {
-//            connectToQueue();
             Session session = connector.getNewSession();
             TextMessage message = session.createTextMessage();
             message.setJMSReplyTo(responseQueue);
             message.setText(text);
 
             switch (queue) {
-            case INTERNAL:
-                getProducer(session, localDbQueue).send(message);
-                break;
-            case INTEGRATION:
-                break;
-            case CONFIG:
-                getProducer(session, configQueue).send(message);
-                break;
+                case INTERNAL:
+                    getProducer(session, localDbQueue).send(message);
+                    break;
+                case INTEGRATION:
+                    break;
+                case CONFIG:
+                    getProducer(session, configQueue).send(message);
+                    break;
             }
 
             return message.getJMSMessageID();
         } catch (Exception e) {
             LOG.error("[ Error when sending message. ] {0}", e.getMessage());
             throw new AuditMessageException("[ Error when sending message. ]", e);
-//        } finally {
-//            disconnectQueue();
         }
     }
 
@@ -89,8 +80,6 @@ public class MessageProducerBean implements MessageProducer, ConfigMessageProduc
         try {
 
             LOG.info("[ Sending message back to recipient on queue ] {}", requestMessage.getJMSReplyTo());
-
-//            connectToQueue();
             Session session = connector.getNewSession();
             TextMessage message = session.createTextMessage();
             message.setJMSCorrelationID(message.getJMSMessageID());
@@ -102,8 +91,6 @@ public class MessageProducerBean implements MessageProducer, ConfigMessageProduc
         } catch (Exception e) {
             LOG.error("[ Error when sending message. ] {}", e.getMessage());
             throw new AuditMessageException("[ Error when sending message. ]", e);
-//        } finally {
-//            disconnectQueue();
         }
     }
 
@@ -112,29 +99,11 @@ public class MessageProducerBean implements MessageProducer, ConfigMessageProduc
     public String sendConfigMessage(String text) throws ConfigMessageException {
         try {
             return sendDataSourceMessage(text, DataSourceQueue.CONFIG);
-        }
-        catch (AuditMessageException e) {
+        } catch (AuditMessageException e) {
             LOG.error("[ Error when sending config message. ] {}", e.getMessage());
             throw new ConfigMessageException("[ Error when sending config message. ]");
         }
     }
-
-//    private void connectToQueue() throws JMSException {
-//        connection = connectionFactory.createConnection();
-//        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//        connection.start();
-//    }
-//
-//    private void disconnectQueue() {
-//        try {
-//            if (connection != null) {
-//                connection.stop();
-//                connection.close();
-//            }
-//        } catch (JMSException e) {
-//            LOG.error("[ Error when closing JMS connection ] {}", e.getMessage());
-//        }
-//    }
 
     private javax.jms.MessageProducer getProducer(Session session, Destination destination) throws JMSException {
         javax.jms.MessageProducer producer = session.createProducer(destination);
